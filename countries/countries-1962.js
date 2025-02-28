6cf299f6fb0d3462be8898ce7213286306479069
needyfemceloverdose <155808859+somehippies@users.noreply.github.com>
2025-02-27 20:58:17 -0600

    Add files via upload

---
+++ countries/countries-1962.js
@@ -0,0 +1,186 @@
+module.exports.countries = [
+    //superpowers
+	['United States', 12400, 200, 60, 0, 'Super Powers', '🇺🇸'],
+	['Nazi Germany', 11400, 320, 60, 0, 'Super Powers', '🇩🇪'],
+	['Japan', 12000, 240, 60, 0, 'Super Powers', '🇯🇵'],
+    
+    //majors
+	['China', 2400, 130, 15, 0, 'Major Powers', '🇹🇼🇯🇵'], //japanese puppet essentially
+	['Italy', 6900, 180, 15, 0, 'Major Powers', '🇮🇹'], //leader of triumvirate
+	
+    
+    //einheitspakt regionals
+    ['United Kingdom', 3100, 15, 5, 0, 'Regional Powers', '🇬🇧'],
+    ['Romania', 1700, 36, 10, 0, 'Regional Powers', '🇷🇴'],
+    
+    //coprosperity regionals
+    ['Azad Hind', 1100, 52, 4, 0, 'Regional Powers', '🇮🇳🇯🇵'],
+    
+    //ofn regionals
+	['Australia', 1400, 29, 4, 0, 'Regional Powers', '🇦🇺'], //fucking hates the j*ps
+	['Brazil', 1700, 24, 1, 0, 'Regional Powers', '🇧🇷'], //observer 
+    ['South Africa', 600, 12, 0, 0, 'Regional Powers', '🇿🇦🇬🇧🇳🇱'],  //not fully ofn aligned until boer war
+    ['Canada', 2800, 34, 9, 0, 'Regional Powers', '🇨🇦'], //exile british government essentially
+    
+    //independent regionals
+    ['Free India', 2400, 53, 3, 0, 'Regional Powers', '🇮🇳'],
+	['Argentina', 1500, 18, 1, 0, 'Regional Powers', '🇦🇷'], //current triumvirate observer
+	['Mexico', 1800, 14, 2, 0, 'Regional Powers', '🇲🇽'], //choose between ofn and coprosperity
+	['Turkey', 1600, 30, 6, 0, 'Regional Powers', '🇹🇷'], //triumvirate member
+	['Iberia', 1300, 24, 4, 0, 'Regional Powers', '🇪🇸🇵🇹'], //triumvirate member
+	['Burgundy', 1300, 89, 25, 0, 'Regional Powers', '🇩🇪🇧🇪'],
+	
+    //einheitspakt minors
+    ['French State', 1900, 5, 0, 0, 'Minor Powers', '🇫🇷'], //forcefully, also crippled 
+    ['Hungary', 1300, 26, 3, 0, 'Minor Powers', '🇭🇺'], //german civil war will leave them alone for a bit
+    ['Bulgaria', 900, 17, 1, 0, 'Minor Powers', '🇧🇬'], //stagnant and dying bulgaria, german civil war will let them free
+    ['RK Norway', 500, 2, 0, 0, 'Minor Powers', '🇳🇴🇩🇪'], //germany occupies Nordstern
+	['Denmark', 600, 2, 0, 0, 'Minor Powers', '🇩🇰🇩🇪'], //Silkeborg garrison presents existential threat to independence
+    ['Ireland', 700, 6, 1, 0, 'Minor Powers', '🇮🇪'], //observer, economic ties to germany disastrous, can easily switch
+    ['RK Ukraine', 700, 17, 0, 0, 'Minor Powers', '🇺🇦🇩🇪'],
+    ['RK Ostland', 700, 15, 0, 0, 'Minor Powers', '🇪🇪🇩🇪'],
+    ['RK Moskowien', 900, 24, 0, 0, 'Minor Powers', '🇷🇺🇩🇪'],
+    ['RK Kaukasien', 500, 14, 0, 0, 'Minor Powers', '🇬🇪🇩🇪'],
+    ['RK Niederlande', 500, 7, 0, 0, 'Minor Powers', '🇳🇱🇩🇪'],
+    ['Generalgouvernement', 500, 11, 0, 'Minor Powers', '🇵🇱🇩🇪'],
+    ['Serbia', 600, 7, 0, 'Minor Powers', '🇷🇸'], //forcefully, german civil war will let them free
+    ['Ostafrika', 200, 5, 0, 0, 'Minor Powers', '🇩🇪'],
+    ['Zentralafrika', 200, 5, 0, 0, 'Minor Powers', '🇩🇪'],
+    ['Sudwestafrika', 200, 5, 0, 0, 'Minor Powers', '🇩🇪'],
+    
+    //coprosperity minors
+    ['Philippines', 400, 6, 0, 0, 'Minor Powers', '🇵🇭🇯🇵'],//japanese puppet in manila and surrounding area
+    ['Manchuria', 800, 11, 0, 0, 'Minor Powers', '🐉🇯🇵'],
+    ['Thai State', 600, 12, 0, 0, 'Minor Powers', '🇹🇭'],
+    ['Indonesia', 700, 10, 0, 0, 'Minor Powers', '🇮🇩'], //unstable
+	['Vietnam', 200, 18, 0, 0, 'Minor Powers', '🇻🇳🇯🇵'], //fighting vietcong
+    
+    
+    //ofn minors
+    ['Venezuela', 500, 6, 0, 0, 'Minor Powers', '🇻🇪'], //unstable af, anti-american sentiment
+    ['New Zealand', 400, 6, 0, 0, 'Minor Powers', '🇳🇿'],
+    ['Peru', 500, 6, 0, 0, 'Minor Powers', '🇵🇪'], //partner
+    
+    
+    //unaligned minors
+    ['Switzerland', 800, 11, 1, 0, 'Minor Powers', '🇨🇭'], //survived alpine crisis, liechtenstein died tho
+    ['Sweden', 900, 18, 2, 0, 'Minor Powers', '🇸🇪'], 
+	['Croatia', 600, 12, 2, 0, 'Minor Powers', '🇭🇷'], //italian puppet
+	['Hellenic State', 800, 6, 0, 0, 'Minor Powers', '🇬🇷🇮🇹'], //italian puppet
+	['Finland', 1100, 16, 3, 0, 'Minor Powers', '🇫🇮'], 
+    ['Free Philippines', 400, 7, 0, 0, 'Minor Powers' '🇵🇭'], //american sponsored anti-japanese southern philippines
+    ['AFRSR', 200, 5, 0, 0, 'Minor Powers' '🇵🇭⚒️'], //commies in north philippines
+	['Saudi Arabia', 300, 3, 0, 0, 'Minor Powers', '🇸🇦'],
+	['Egypt', 600, 12, 0, 0, 'Minor Powers', '🇪🇬🇮🇹'], //italian puppet
+	['Iraq', 300, 5, 0, 0, 'Minor Powers', '🇮🇶'], //heavy italian influence
+	['Cuba', 300, 6, 0, 0, 'Minor Powers', '🇨🇺'], //castro, not neccesarily communist
+	['Chile', 600, 6, 0, 0, 'Minor Powers', '🇨🇱'], //ofn and coprosperity compete over influence
+	['Colombia', 400, 6, 0, 0, 'Minor Powers', '🇨🇴'], //iberian supported
+    ['Colombian Revolutionaries', 300, 3, 0, 0, 'Minor Powers', '🇨🇴⚒️'], //ofn supported
+    ['New Grenada', 300, 2, 0, 0, 'Minor Powers', '🇨🇴🏴'], //i think italian supported?
+	['Bolivia', 400, 4, 0, 0, 'Minor Powers', '🇧🇴'], //embarassed from loss to paraguay
+	['Uruguay', 400, 3, 0, 0, 'Minor Powers', '🇺🇾'], //ofn partner, friends with brazil
+	['Afghanistan', 600, 14, 0, 0, 'Minor Powers', '🇦🇫'], //took a bunch of pakistan
+
+	['Iceland', 300, 2, 0, 0, 'Insignificant Powers', '🇮🇸'],
+	['Tibet', 200, 1, 0, 0, 'Insignificant Powers', '🛕'],
+    ['NRA 24th Army', 200, 4, 0, 0, 'Insignificant Powers', '🇹🇼'],
+    ['Xinjiang BDA', 300, 6, 0, 0, 'Insignificant Powers', '🇹🇼'],
+    ['Jinshaan', 400, 8, 0, 0, 'Insignificant Powers', '🇹🇼🇯🇵'],
+    ['Guangdong', 900, 2, 0, 0, 'Insignificant Powers' '🇹🇼🇯🇵'],
+	['Nepal', 200, 2, 0, 0, 'Insignificant Powers', '🇳🇵'],
+	['Mengjiang', 300, 4, 0, 0, 'Insignificant Powers', '🇲🇳🇯🇵'],
+	['Oman', 100, 1, 0, 0, 'Insignificant Powers', '🇴🇲'],
+	['Yemen', 100, 1, 0, 0, 'Insignificant Powers', '🇾🇪'],
+	['Levant', 400, 7, 0, 0, 'Insignificant Powers', '🇯🇴🇮🇹'],
+	['Albania', 200, 4, 0, 0, 'Insignificant Powers', '🇦🇱🇮🇹'],
+	['Ecuador', 300, 3, 0, 0, 'Insignificant Powers', '🇪🇨'],
+	['Paraguay', 300, 6, 0, 0, 'Insignificant Powers', '🇵🇾'],
+    ['Santa Cruz', 100, 4, 0, 0, 'Insignificant Powers', '🇧🇴🇵🇾'],
+	['Guatemala', 200, 3, 0, 0, 'Insignificant Powers', '🇬🇹'],
+	['El Salvador', 200, 3, 0, 0, 'Insignificant Powers', '🇸🇻'],
+	['Honduras', 200, 3, 0, 0, 'Insignificant Powers', '🇭🇳'],
+	['Nicaragua', 200, 3, 0, 0, 'Insignificant Powers', '🇳🇮'],
+	['Costa Rica', 200, 3, 0, 0, 'Insignificant Powers', '🇨🇷'],
+	['Panama', 200, 3, 0, 0, 'Insignificant Powers', '🇵🇦'],
+	['Dominican Republic', 300, 8, 0, 0, 'Insignificant Powers', '🇩🇴'],
+    ['Puerto Plata', 0, 2, 0, 0, 'Insignificant Powers', '🇩🇴']
+	['Haiti', 200, 5, 0, 0, 'Insignificant Powers', '🇭🇹'],
+	['Liberia', 200, 1, 0, 0, 'Insignificant Powers', '🇱🇷'],
+    ['Northern Ireland', 100, 3, 0, 0, 'Insignificant Powers', '🇮🇪'],
+    ['Iberian Algeria', 100, 3, 0, 0, 'Insignificant Powers', '🇩🇿🇪🇸'],
+    ['Italian Algeria', 100, 3, 0, 0, 'Insignificant Powers', '🇩🇿🇮🇹'],
+    ['Mosul and Kerkuk', 100, 2, 0, 0, 'Insignificant Powers', '🇮🇶🇹🇷'],
+	['Syria', 200, 3, 0, 0, 'Insignificant Powers', '🇸🇾'],
+    ['Vietcong', 100, 27, 0, 0, 'Insignificant Powers', '🇻🇳'],
+    ['Kampuchea', 100, 2, 0, 0, 'Insignificant Powers', '🇰🇭'],
+    ['Laos', 100, 2, 0, 0, 'Insignificant Powers', '🇱🇦'],
+    ['Montenegro', 100, 2, 0, 0, 'Insignificant Powers', '🇮🇲🇮🇹']
+    ['Seihoku Homengun', 200, 4, 0, 0, 'Insignificant Powers', '🇹🇼🇯🇵'],
+    ['Yunnan Province', 200, 4, 0, 0, 'Insignificant Powers', '🇹🇼🇯🇵'],
+    ['Guizhou Province', 200, 4, 0, 0, 'Insignificant Powers', '🇹🇼🇯🇵'],
+    ['Guangxi Province', 200, 4, 0, 0, 'Insignificant Powers', '🇹🇼🇯🇵'],
+    ['Myanmar', 300, 5, 0, 0, 'Insignificant Powers', '🇲🇲🇯🇵']
+    ['Malaya', 200, 7, 0, 0, 'Insignificant Powers', '🇲🇾🇯🇵'],
+    ['UMAJF', 100, 16, 0, 0, 'Insignificant Powers', '🇲🇾⚒️'],
+    ['Sarawak', 200, 7, 0, 0, 'Insignificant Powers', '🇲🇾🇧🇳'],
+    ['Ceylon', 100, 3, 0, 0, 'Insignificant Powers', '🇱🇰'],
+    ['Kalat', 100, 3, 0, 0, 'Insignificant Powers', '🇦🇫🇵🇰'],
+    ['Italian Gulf', 100, 1, 0, 0, 'Insignificant Powers', '🇦🇪🇮🇹']
+    ['Italian East Africa', 300, 8, 0, 0, 'Insignificant Powers', '🇪🇹🇮🇹']
+    
+	['San Marino', 100, 0, 0, 0, 'Microstates', '🇸🇲🇮🇹'],
+	['Vatican City State', 100, 0, 0, 0, 'Microstates', '🇻🇦'],
+	['Monaco', 100, 0, 0, 0, 'Microstates', '🇲🇨🇮🇹'],
+	['Andorra', 100, 0, 0, 0, 'Microstates', '🇦🇩'],
+	['Bhutan', 200, 1, 0, 0, 'Microstates', '🇧🇹🇮🇳🇯🇵'],
+    
+    
+    //russian warlords west russia
+    ['WRRF', 500, 14, 0, 0, 'Minor Power', '🇷🇺⚒️'], //strongest in west russia
+	['Onega', 100, 5, 0, 0, 'Insignificant Power', '🇷🇺'], //finnish supported anti-communists
+    ['Komi', 400, 12, 0, 0, 'Insignificant Powers', '🇷🇺'],
+    ['Vologda', 300, 12, 0, 0, 'Insignificant Powers', '🇷🇺'], //"neutral zone"
+    ['Gorky', 200, 4, 0, 0, 'Insignificant Powers', '🇷🇺'], //commie
+    ['Tatarstan', 200, 5, 0, 0, 'Insignificant Powers', '🇷🇺☪️'], //doesnt like russia
+    ['Samara', 400, 11, 0, 0, 'Insignificant Powers', '🇷🇺'], //german bootlickers
+    ['Vyatka', 400, 12, 0, 0, 'Insignificant Powers', '🇷🇺'], //tsar vladimir
+    ['Aryan Brotherhood', 200, 5, 0, 0, 'Insignificant Powers', '🇷🇺🇩🇪'], //schizo russians that think they can be aryan too
+    ['Gayny', 100, 3, 0, 0, 'Insignificant Powers', '🇷🇺☦️'], //orthodox warrior order
+    ['Bashkortostan', 200, 4, 0, 0, 'Insignificant Powers', '🇷🇺☪️'], //doesnt like russia
+    
+    //urals (can be taken by either west russia or west siberia once united)
+    ['Orenburg', 200, 6, 0, 0, 'Insignificant Powers', '🇷🇺'], //Russian ancoms
+    ['Ural League', 200, 5, 0, 0, 'Insignificant Powers', '🇷🇺'], //russian dictatorship
+    ['Magnitogorsk', 100, 4, 0, 0, 'Insignificant Powers', '🇷🇺'], //russian dictatorship led by crazy scientist
+    ['Dirlewanger', 100, 3, 0, 0, 'Insignificant Powers', '🇩🇪'] //wacko nazi raiders by kazakhstan
+    
+    
+    //russian warlords west siberia
+    ['Vorkuta', 100, 2, 0, 0, 'Insignificant Powers', '🇷🇺'], //wacko nkvd labor camp
+    ['Yugra', 200, 4, 0, 0, 'Insignificant Powers', '🇷🇺'], //thieving entrepeneurs
+    ['Free Aviators', 100, 0, 0, 0, 'Insignificant Powers', '🇷🇺✈️'], //soviet aviators that counteract luftwaffe
+    ['Sverdlovsk', 300, 9, 0, 0, 'Insignificant Powers', '🇷🇺'], //dictatorship with black league and commie influence
+    ['Tyumen', 400, 13, 0, 0, 'Minor Powers', '🇷🇺⚒️'], //commies in west siberia, marxist-leninist
+    ['Black League', 400, 14, 0, 0, 'Insignificant Powers', '🇷🇺'], //army clique dedicated to killing the germans and nazism
+    ['Zlatoust', 300, 8, 0, 0, 'Insignificant Powers', '🇷🇺'], //arms dealers that sell their army for cash, profiting off war
+    
+    //russian warlords central siberia
+    ['Tomsk', 500, 7, 0, 0, 'Insignificant Powers', '🇷🇺'], //a sort of technocratic republic
+    ['Novosibirsk', 500, 9, 0, 0 'Minor Powers', '🇷🇺'], //you know him you love him, the federation of shukshin
+    ['Oryotia', 200, 5, 0, 0 'Insignificant Powers', '🇷🇺☦️'], //orthodox clique
+    ['Kemerevo', 300, 6, 0, 0, 'Insignificant Powers', '🇷🇺'], //wack ass esoteric monarchy
+    ['Krasnoyarsk', 300, 8, 0, 0, 'Insignificant Powers', '🇷🇺'], //military "guided" democracy
+    ['Black Army', 300, 6, 0, 0, 'Insignificant Powers', '🇷🇺🏴'], //anarchists
+    ['Tannu Tuva', 200, 5, 0, 0, 'Insignificant Powers', '🇷🇺🇲🇳'], //not technically involved but doomed to be swallowed
+    
+    //russian warlords east siberia
+    ['Irkutsk', 500, 10, 0, 0, 'Minor Powers', '🇷🇺⚒️'], //nkvd
+    ['Buryatia', 300, 6, 0, 0, 'Insignificant Powers', '🇷🇺⚒️'], //idealistic communists
+    ['Chita', 300, 6, 0, 0, 'Insignificant Powers', '🇷🇺⚒️'], //idealistic communists
+    ['Amur', 300, 5, 0, 0, 'Insignificant Powers', '🇷🇺'], //nazis
+    ['Magadan', 300, 7, 0, 0, 'Insignificant Powers', '🇷🇺'], //fascists but not nazis
+    ['Sakha', 200, 2, 0, 0, 'Insignificant Powers', '🇷🇺'], //new republic? no fucking clue what they do
+    ['Kamchatka', 100, 4, 0, 0, 'Insignificant Powers', '🇷🇺⚓️'], //soviet navy remnants
+    
+    ]
